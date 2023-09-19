1b318f605a5fb205c37012fceccbb23785e3f297
George-Itegi <georgeitegi2001@gmail.com>
2023-09-19 22:40:41 +0300

    new files in

--- _putchar.c
+++ _putchar.c
@@ -1,30 +1,11 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - print char with stdout
- * @ch: char to print
- * Return: Output.
- */
-int _putchar(char ch)
-{
-	static int contador;
-	static char buffer[1024];
+ * putchar  writes the character c to stdout
 
-	if (ch == -1)
-	{
-		contador = 0;
-		return (0);
-	}
-	if (ch == -2 || contador == 1024)
-	{
-		write(1, buffer, contador);
-		contador = 0;
-	}
-	if (ch != -1 && ch != -2)
-	{
-		buffer[contador] = ch;
-		contador++;
-		return (1);
-	}
-	return (0);
+ * On error -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
